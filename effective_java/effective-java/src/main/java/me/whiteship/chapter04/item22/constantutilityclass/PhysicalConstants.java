c13246f0cb28009e75002aac64e6b3e0bf92e847
김태민 <seolin1218@gmail.com>
2023-04-29 20:45:27 +0900

    아이템 22 인터페이스는 타입을 정의하는 용도로만 사용하라

---
+++ effective_java/effective-java/src/main/java/me/whiteship/chapter04/item22/constantutilityclass/PhysicalConstants.java
@@ -0,0 +1,14 @@
+package me.whiteship.chapter04.item22.constantutilityclass;
+
+public final class PhysicalConstants {
+    private PhysicalConstants() {
+    }
+
+    public static final double AVOGADROS_NUMBER   = 6.022_140_857e23;
+
+    // 볼츠만 상수 (J/K)
+    public static final double BOLTZMANN_CONSTANT = 1.380_648_52e-23;
+
+    // 전자 질량 (kg)
+    public static final double ELECTRON_MASS      = 9.109_383_56e-31;
+}
